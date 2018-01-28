ba4efca2ad4385486c5fa7ab1bfb1c89eecf5b32
Jamal Rasool <vevecorp@users.noreply.github.com>
2018-01-28 02:26:45 -0600

    Updated Comments

--- PhysVec.cpp
+++ PhysVec.cpp
@@ -1,9 +1,8 @@
 /**
  * Purpose of the program is to use to the PhysVec class and implement the functions that are given
- * @author Jamal Rasool, Gese Rocha, John Daloni, Fredy Martinez
+ * @authors Jamal Rasool, Gese Rocha, John Daloni, Fredy Martinez
  * @version 1.1
  * @date 1/25/18
- * @
  */
 
 
@@ -15,7 +14,7 @@
  * Function Name: PhysVec
  * Declaring the default constructor, typically will be empty.
  */
-PhysVec::PhysVec(){
+PhysVec::PhysVec() {
 }
 
 /**
@@ -24,7 +23,7 @@ PhysVec::PhysVec(){
  * @param input_vec this parameter is designed to allow for the program to pass through vectors in order to place it
  * into the PhysVec Object.
  */
-PhysVec::PhysVec(const std::vector<int> & input_vec) : a_vec_(input_vec) {
+PhysVec::PhysVec(const std::vector<int> &input_vec) : a_vec_(input_vec) {
 }
 
 /**
@@ -34,10 +33,10 @@ PhysVec::PhysVec(const std::vector<int> & input_vec) : a_vec_(input_vec) {
  * PhyscVec object.
  */
 
-PhysVec::PhysVec(const PhysVec & rhs)  {
+PhysVec::PhysVec(const PhysVec &rhs) {
     try {
         this->a_vec_ = rhs.a_vec_; // setting the temporary "this" to the rhs object that was passed through.
-    }catch  (int k){
+    } catch (int k) {
         throw "Program has critically failed.";
     }
 }
@@ -50,16 +49,17 @@ PhysVec::PhysVec(const PhysVec & rhs)  {
  * @return the operator+ function will return the temporary third vector back to be read using the printVecs() function
  * call.
  */
-PhysVec PhysVec :: operator+ (const PhysVec  & rhs) const {
 
-    std:: vector <int> :: const_iterator a; // Setting up the default constant iterator that can't be modified
-    std :: vector <int> :: const_iterator b;
-    std :: vector <int> :: iterator c;
+PhysVec PhysVec::operator+(const PhysVec &rhs) const {
+
+    std::vector<int>::const_iterator a; // Setting up the default constant iterator that can't be modified
+    std::vector<int>::const_iterator b;
+    std::vector<int>::iterator c;
 
     a = rhs.a_vec_.begin(); // Setting begining
     b = this->a_vec_.begin();
 
-    std :: vector<int> t;
+    std::vector<int> t;
 
     int size = this->a_vec_.size();
 
@@ -68,19 +68,20 @@ PhysVec PhysVec :: operator+ (const PhysVec  & rhs) const {
 
     // Testing the vector addition, by going through and dereferencing all of the vectors.
     try {
-        for(c; c!= t.end(); ++c) {
+        for (c; c != t.end(); ++c) {
             *c = *a + *b;
 
             ++b;
             ++a;
         }
-    }catch (std::exception e) {
+    } catch (std::exception e) {
         throw e; // Throwing the exception error for any error.
     }
 
     PhysVec temporary_object(t);
     return temporary_object;
 }
+
 /**
  * Function Name: setPhysVec
  * This function is to act as a setter for a PhysVec. It is used with the default constructor which creates a non
@@ -89,7 +90,7 @@ PhysVec PhysVec :: operator+ (const PhysVec  & rhs) const {
  * into the PhysVec Object.
  */
 
-void PhysVec::setPhysVec(const std::vector<int> & input_vec) {
+void PhysVec::setPhysVec(const std::vector<int> &input_vec) {
     a_vec_ = input_vec; // setting the private member variable of PhysVec to the passed through vector.
 }
 
@@ -99,7 +100,7 @@ void PhysVec::setPhysVec(const std::vector<int> & input_vec) {
  * @return will return a vector.
  */
 
-std::vector<int> &PhysVec :: get_a_vec_() {
+std::vector<int> &PhysVec::get_a_vec_() {
     return a_vec_;
 }
 
@@ -113,15 +114,15 @@ std::vector<int> &PhysVec :: get_a_vec_() {
  * @return This function was designed to return the total product of what the dot multiplication would be.
  */
 
-int PhysVec::calculateDotProduct(const PhysVec & rhs) const {
+int PhysVec::calculateDotProduct(const PhysVec &rhs) const {
     int product = 0;
 
-    std :: vector<int> :: const_iterator DefaultIterator;
-    std :: vector<int> :: const_iterator b;
+    std::vector<int>::const_iterator DefaultIterator;
+    std::vector<int>::const_iterator b;
 
     b = this->a_vec_.begin();
 
-    for(DefaultIterator = rhs.a_vec_.begin(); DefaultIterator != rhs.a_vec_.end(); ++DefaultIterator) {
+    for (DefaultIterator = rhs.a_vec_.begin(); DefaultIterator != rhs.a_vec_.end(); ++DefaultIterator) {
 
         product += *DefaultIterator * *b;
         ++b;
@@ -138,10 +139,10 @@ int PhysVec::calculateDotProduct(const PhysVec & rhs) const {
 
 void PhysVec::printVecs() const {
 
-    for(auto lhs = this->a_vec_.begin(); lhs != this->a_vec_.end(); ++lhs)
-    {
-        std :: cout.flush() << " " << *lhs;
-    }}
+    for (auto lhs = this->a_vec_.begin(); lhs != this->a_vec_.end(); ++lhs) {
+        std::cout.flush() << " " << *lhs;
+    }
+}
 
 /**
  * Function Name: operator=
@@ -154,14 +155,14 @@ void PhysVec::printVecs() const {
  */
 
 PhysVec &PhysVec::operator=(const PhysVec &rhs) {
-    if(this != &rhs) {
-        std :: vector<int> :: const_iterator defaultiterator; // Acting as the right hand side of the progression, will
+    if (this != &rhs) {
+        std::vector<int>::const_iterator defaultiterator; // Acting as the right hand side of the progression, will
         // not go up
-        std :: vector<int> :: iterator lhs;
+        std::vector<int>::iterator lhs;
 
         lhs = this->a_vec_.begin();
 
-        for(defaultiterator = rhs.a_vec_.begin(); defaultiterator != rhs.a_vec_.end(); ++defaultiterator) {
+        for (defaultiterator = rhs.a_vec_.begin(); defaultiterator != rhs.a_vec_.end(); ++defaultiterator) {
             *lhs = *defaultiterator;
             lhs++;
         }
--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
  * @version 1.1
  * @brief This program is designed to go through and test all of the constructors / functions that are available in the
  * PhysVec header file and driver file.
+ * @date January 28th, 2017
  */
 
 #include <iostream>
@@ -10,53 +11,59 @@
 
 /**
  * Function Name: TestingPrintVecs()
- * Designed to test the printing of functions inside of the
+ * Designed to test the printing of functions inside of the PhysVec.cpp file; also by doing this we are also testing to
+ * see if the setPhysVec function works as properly.
  */
 
 void TestingPrintVecs() {
-    std :: cout << "Test One:" << std :: endl;
-    std :: vector<int> MyVectorA {4,2,1,8,9,10};
-    std :: vector<int> MyVectorB {1,2,5,24,19};
+    std::cout << "Test One (Testing Printing Vectors):" << std::endl;
+    std::vector<int> MyVectorA{4, 2, 1, 8, 9, 10};
+    std::vector<int> MyVectorB{1, 2, 5, 24, 19};
     try {
         PhysVec MyVectorObject;
         MyVectorObject.setPhysVec(MyVectorA);
         MyVectorObject.printVecs();
-    }catch(std::exception e) {
-        throw  e;
+    } catch (std::exception e) {
+        throw e;
     }
-    std :: cout << "\nTest One is Complete!" << std :: endl;
+    std::cout << "\nTest One is Complete!" << std::endl;
 }
 
 /**
  * Function Name: TestingConstructor
- *
+ * This function is designed to go through and test with a direct call to the constructor to place that vector within a
+ * Physvec object without needing to call the setPhysVec function call.
  */
 
 void TestingConstructor() {
     // use regular constructor
-    std::vector<int> TestVector = {42,112,421};
-    std :: cout << "\nTest Two:" << std :: endl;
+    std::vector<int> TestVector = {42, 112, 421};
+    std::cout << "\nTest Two (Testing Constructor):" << std::endl;
     try {
         PhysVec myphysvec2(TestVector);
         myphysvec2.printVecs();
-    }catch (std::exception e) {
+    } catch (std::exception e) {
         throw e;
     }
-    std :: cout << "\nTest Two Complete!" << std :: endl;
+    std::cout << "\nTest Two Complete!" << std::endl;
 }
 
 /**
- *
+ * Function name: TestingAddition
+ * Designed to go through and test the addition constructors within the PhysVec class, as designed it will scale down
+ * the bigger vector size down to the smallest of what is being added together. This is to stop overflow / overloading
+ * of the program.
  */
 
 void TestingAddition() {
 
-    std :: cout << "\nTest Three:" << std :: endl;
+    std::cout << "\nTest Three (Testing Addition):" << std::endl;
 
-    std::vector<int> Vec {0,0,0,0,0};
-    std::vector<int> Veca {1,1,1,1,1};
-    std::vector<int> Vecb {2,2,2,2,2};
-    std::vector<int> Vecc {3,3,3,3,3,3}; // Designed to check and see what it throws when the array size is out of bounds
+    std::vector<int> Vec{0, 0, 0, 0, 0};
+    std::vector<int> Veca{1, 1, 1, 1, 1};
+    std::vector<int> Vecb{2, 2, 2, 2, 2};
+    std::vector<int> Vecc{3, 3, 3, 3, 3,
+                          3}; // Designed to check and see what it throws when the array size is out of bounds
 
     PhysVec BaseObject(Vec);
     PhysVec BaseOne(Veca);
@@ -64,103 +71,107 @@ void TestingAddition() {
     PhysVec BaseThree(Vecc);
 
     // testing the overloading of the addition and the = operators with both vectors being the same size.
-    BaseObject =  BaseOne + BaseTwo;
+    BaseObject = BaseOne + BaseTwo;
     BaseObject.printVecs();
 
     // Testing the overloading of addition when both of the vectors are not the same size;
     try {
         BaseObject = BaseOne + BaseThree;
         BaseObject.printVecs();
-    }catch (std::exception e) {
+    } catch (std::exception e) {
         throw e;
     }
-    std :: cout << "\nTest Three Complete!" << std :: endl;
+    std::cout << "\nTest Three Complete!" << std::endl;
 }
 
 /**
  * Function Name: TestingSimpleDotProduct
- *
+ * Designed to go through and perform a simple dot product without the use of negative integers.
  */
 
 void TestingSimpleDotProduct() {
-    std :: cout << "\nTest Four:" << std :: endl;
+    std::cout << "\nTest Four (Testing Simple Dot Product):" << std::endl;
 
-    std::vector<int> Set1 {5,8,13,16,21};
-    std::vector<int> Set2 {4,11,15,24,28};
+    std::vector<int> Set1{5, 8, 13, 16, 21};
+    std::vector<int> Set2{4, 11, 15, 24, 28};
     PhysVec ObjectSet1(Set1);
     PhysVec ObjectSet2(Set2);
 
     //Testing Simple Dot Product
     try {
-        int dotProduct = ObjectSet1.calculateDotProduct(ObjectSet2);
-        std :: cout << "Dot product is " << dotProduct << std :: endl;
-    }catch (std::exception e) {
+        int dotProduct = ObjectSet1.calculateDotProduct(ObjectSet2); // Calculation
+        std::cout << "Dot product is " << dotProduct << std::endl; // Returning values
+    } catch (std::exception e) {
         throw e;
     }
 
-    std :: cout << "Test Four Complete!" << std :: endl;
+    std::cout << "Test Four Complete!" << std::endl;
 }
 
 /**
  * Function Name: TestingComplexDotProduct
- *
+ * Function is designed to go through with positive and negative integers and first display the numbers that we will use
+ * in the dot matrix then return the values of the Dot multiplication.
  */
 
 void TestingComplexDotProduct() {
-    std :: cout << "\nTest Five:" << std :: endl;
+    std::cout << "\nTest Five (Testing Complex Dot Product):" << std::endl;
 
-    std::vector<int> biggervec1 = {1,2,3,4,5};
-    std::vector<int> biggervec2 = {-1,-2,-3,-4,-5};
-    std::vector<int> biggervec3 {5,6,7,8,9,19,11,12};
+    std::vector<int> biggervec1 = {1, 2, 3, 4, 5};
+    std::vector<int> biggervec2 = {-1, -2, -3, -4, -5};
+    std::vector<int> biggervec3{5, 6, 7, 8, 9, 19, 11, 12};
 
     PhysVec mybigvec1(biggervec1);
     PhysVec mybigvec2(biggervec2);
     PhysVec mybigvec3(biggervec3);
 
-    std :: cout << "Vector Values" << std :: endl;
+    // Displaying the information inside of the vectors
+    std::cout << "Vector Values" << std::endl;
     mybigvec1.printVecs();
-    std :: cout << " | ";
+    std::cout << " | ";
     mybigvec2.printVecs();
-    std :: cout << " | ";
+    std::cout << " | ";
     mybigvec3.printVecs();
 
-// Testing original case where it will multiple the vector sizes that are the exact same size.
+    // Testing original case where it will multiple the vector sizes that are the exact same size.
     int dotProduct = mybigvec2.calculateDotProduct(mybigvec1);
-    std :: cout << std :: endl;
-    std :: cout << "Dot product w/ same size is " << dotProduct;
-// try unequal vectors in dot product
+    std::cout << std::endl;
+    std::cout << "Dot product w/ same size is " << dotProduct;
+
+    // try unequal vectors in dot product
     try {
-        dotProduct = mybigvec3.calculateDotProduct(mybigvec2);
-        std :: cout << std :: endl;
-        std :: cout << "Dot product w/o same size is " << dotProduct;
-    }catch (std::invalid_argument e) {
+        dotProduct = mybigvec3.calculateDotProduct(mybigvec1);
+        std::cout << std::endl;
+        std::cout << "Dot product w/o same size is " << dotProduct;
+    } catch (std::invalid_argument e) {
         throw e;
     }
-    std :: cout << "\nTest Five Complete!" << std :: endl;
+    std::cout << "\nTest Five Complete!" << std::endl;
 }
 
 /**
  * Function Name: TestingCopyConstructor
- *
+ * Designed to test and see if the copyconstructor copies the information from vector another. But first displaying the
+ * information that is in the original vector, and then displaying the information that is in the new temporary vector
+ * that we have created.
  */
 
 void TestingCopyConstructor() {
-    std :: cout << "\nTest Six:" << std :: endl;
+    std::cout << "\nTest Six (Testing Copy Constructor):" << std::endl;
     //test copy constructor
-    std::vector<int> biggervec2 = {-1,-2,-3,-4,-5};
+    std::vector<int> biggervec2 = {-1, -2, -3, -4, -5};
     PhysVec sad(biggervec2);
 
     PhysVec happy(sad);
-    std :: cout << "Expected: -1,-2,-3,-4,-5" << std :: endl;
-    std :: cout << "Actual: ";
+    std::cout << "Expected: -1,-2,-3,-4,-5" << std::endl;
+    std::cout << "Actual: ";
     happy.printVecs();
-
-    std :: cout << "\nTest Six Complete!" << std :: endl;
+    std::cout << "\nTest Six Complete!" << std::endl;
 }
 
 /**
- * Funciton Name: main
- *
+ * Function Name: main
+ * Designed to go through and run all of the functions of the program.
  * @return
  */
 
@@ -179,5 +190,5 @@ int main() {
     // Testing Copy Constructor
     TestingCopyConstructor();
 
-  return 0;
+    return 0;
 }
